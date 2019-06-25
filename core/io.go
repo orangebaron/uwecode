3125299c9d41ce9b43a0004ad7ffd53e274c0fc7
orangebaron <sam.alws@gmail.com>
2019-06-25 14:02:05 -0400

    capitalize process names, DoneWithProcess, pids as string

--- core/io.go
+++ core/io.go
@@ -1,21 +1,22 @@
 package core
 
 type Controller interface {
-	giveInput([]byte) Obj
-	takeOutput([]byte, Obj)
-	initNewProcess() []byte
+	GiveInput(string) Obj
+	TakeOutput(string, Obj)
+	InitNewProcess() string
+	DoneWithProcess(string)
 }
 
 type IO interface {
-	run(Controller, []byte) (Obj, bool)
+	run(Controller, string) (Obj, bool)
 }
 
 type InputIO struct {
 	Obj
 }
 
-func (io InputIO) run(controller Controller, pid []byte) (Obj, bool) {
-	return io.Obj.Call(controller.giveInput(pid)), true
+func (io InputIO) run(controller Controller, pid string) (Obj, bool) {
+	return io.Obj.Call(controller.GiveInput(pid)), true
 }
 
 type OutputIO struct {
@@ -23,8 +24,8 @@ type OutputIO struct {
 	nextObj Obj
 }
 
-func (io OutputIO) run(controller Controller, pid []byte) (Obj, bool) {
-	controller.takeOutput(pid, io.otp)
+func (io OutputIO) run(controller Controller, pid string) (Obj, bool) {
+	controller.TakeOutput(pid, io.otp)
 	return io.nextObj, true
 }
 
@@ -33,19 +34,23 @@ type ForkIO struct {
 	objB Obj
 }
 
-func (io ForkIO) run(controller Controller, _ []byte) (Obj, bool) {
-	go runProcess(io.objB, controller)
+func (io ForkIO) run(controller Controller, _ string) (Obj, bool) {
+	c := make(chan struct{})
+	go RunProcess(io.objB, controller, c)
+	<-c
 	return io.objA, true
 }
 
 type NullIO struct{}
 
-func (_ NullIO) run(_ Controller, _ []byte) (Obj, bool) {
+func (_ NullIO) run(_ Controller, _ string) (Obj, bool) {
 	return ReturnVal{0}, false
 }
 
-func runProcess(f Obj, controller Controller) {
-	pid := controller.initNewProcess()
+func RunProcess(f Obj, controller Controller, closeWhenInitted chan struct{}) {
+	pid := controller.InitNewProcess()
+	close(closeWhenInitted)
 	for keepGoing := true; keepGoing; f, keepGoing = ObjToIO(f).run(controller, pid) {
 	}
+	controller.DoneWithProcess(pid)
 }
