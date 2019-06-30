7c854600915eba4d51cd1a8e3e34c04c75876fa8
orangebaron <sam.alws@gmail.com>
2019-06-30 09:32:21 -0400

    better structured code

--- compiler/codeReader.go
+++ compiler/codeReader.go
@@ -86,55 +86,44 @@ func NormalCallWithinParens(b byte, inParens NormalReaderState) *NormalReaderSta
 
 func NormalReader(b byte, state interface{}, decls []Declaration) (interface{}, []Declaration, CharacterReader, EOFFunction) {
 	convertedState := state.(NormalReaderState)
-	if IsWhitespace(b) {
-		if convertedState.CurrentWord == "=" {
-			convertedState.NormalDeclaration.Expression = convertedState.LastExpression
-			newState := WhitespaceReaderState{NormalReaderState{NullExpression{}, NullExpression{}, nil, "", "", NormalDeclaration{convertedState.LastWord, NullExpression{}}}, NormalReader, NormalEOFFunction}
-			if convertedState.NormalDeclaration.Name != "" {
-				decls = append(decls, convertedState.NormalDeclaration)
-			}
-			return newState, decls, WhitespaceReader, ErrorEOFFunction
-		} else {
-			if convertedState.InParentheses == nil {
+	// TODO: check for errors: too many )s, = within paren, delaring something as nothing: "a = 2 b = c = 5"
+	if IsWhitespace(b) && convertedState.CurrentWord == "=" {
+		convertedState.NormalDeclaration.Expression = convertedState.LastExpression
+		newState := WhitespaceReaderState{NormalReaderState{NullExpression{}, NullExpression{}, nil, "", "", NormalDeclaration{convertedState.LastWord, NullExpression{}}}, NormalReader, NormalEOFFunction}
+		if convertedState.NormalDeclaration.Name != "" {
+			decls = append(decls, convertedState.NormalDeclaration)
+		}
+		return newState, decls, WhitespaceReader, ErrorEOFFunction
+	} else {
+		isSpecial := IsWhitespace(b) || b == '(' || b == ')'
+		if convertedState.InParentheses == nil {
+			if isSpecial && convertedState.CurrentWord != "" {
 				convertedState.LastExpression = convertedState.Expression
 				convertedState.Expression = convertedState.Expression.AddWordToEnd(convertedState.CurrentWord)
 				convertedState.LastWord = convertedState.CurrentWord
 				convertedState.CurrentWord = ""
-			} else {
-				convertedState.InParentheses = NormalCallWithinParens(b, *convertedState.InParentheses)
 			}
+			if !isSpecial {
+				convertedState.CurrentWord = convertedState.CurrentWord + string(b)
+			}
+			if b == '(' {
+				newEnclosedNormState := NormalReaderState{NullExpression{}, NullExpression{}, nil, "", "", NormalDeclaration{"", NullExpression{}}}
+				convertedState.InParentheses = &newEnclosedNormState
+			}
+		} else {
+			convertedState.InParentheses = NormalCallWithinParens(b, *convertedState.InParentheses)
+			if b == ')' && convertedState.InParentheses.InParentheses == nil {
+				convertedState.LastExpression = convertedState.Expression
+				convertedState.Expression = convertedState.Expression.AddExpressionToEnd(ParenExpression{convertedState.InParentheses.Expression})
+				convertedState.InParentheses = nil
+			}
+		}
+
+		if isSpecial {
 			return WhitespaceReaderState{convertedState, NormalReader, NormalEOFFunction}, decls, WhitespaceReader, NormalEOFFunction
-		}
-	} else if b == '(' {
-		// assuming that theres a space before open paren
-		// TODO: don't assume that
-		if convertedState.InParentheses == nil {
-			newEnclosedNormState := NormalReaderState{NullExpression{}, NullExpression{}, nil, "", "", NormalDeclaration{"", NullExpression{}}}
-			convertedState.InParentheses = &newEnclosedNormState
 		} else {
-			convertedState.InParentheses = NormalCallWithinParens(b, *convertedState.InParentheses)
+			return convertedState, decls, NormalReader, NormalEOFFunction
 		}
-		return WhitespaceReaderState{convertedState, NormalReader, NormalEOFFunction}, decls, WhitespaceReader, ErrorEOFFunction
-	} else if b == ')' {
-		// assuming that there's a space before closing paren
-		// TODO: don't assume that
-		// also assuming that InParentheses != nil, ie something didnt go very badly wrong
-		// TODO: don't assume that either
-		if convertedState.InParentheses.InParentheses == nil {
-			convertedState.LastExpression = convertedState.Expression
-			convertedState.Expression = convertedState.Expression.AddExpressionToEnd(ParenExpression{convertedState.InParentheses.Expression})
-			convertedState.InParentheses = nil
-		} else {
-			convertedState.InParentheses = NormalCallWithinParens(b, *convertedState.InParentheses)
-		}
-		return WhitespaceReaderState{convertedState, NormalReader, NormalEOFFunction}, decls, WhitespaceReader, NormalEOFFunction
-	} else {
-		if convertedState.InParentheses == nil {
-			convertedState.CurrentWord = convertedState.CurrentWord + string(b)
-		} else {
-			convertedState.InParentheses = NormalCallWithinParens(b, *convertedState.InParentheses)
-		}
-		return convertedState, decls, NormalReader, NormalEOFFunction
 	}
 }
 
